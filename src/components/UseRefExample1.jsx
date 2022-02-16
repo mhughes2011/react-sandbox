cbb0bcc7925c4f5686bce84b35f4574b778604b7
Max Hughes <mhughes2011@gmail.com>
2022-02-15 18:38:22 -0800

    removed all answers to start from scratch and did useRef hook example

--- src/components/UseRefExample1.jsx
+++ src/components/UseRefExample1.jsx
@@ -1,36 +1,32 @@
-import { useRef } from 'react'
+import React, {useRef} from 'react'
 
 function UseRefExample1() {
-  const inputRef = useRef()
-  const paraRef = useRef()
+    const inputRef = useRef()
+    const paraRef = useRef()
+    // inputRef is the object of whatever DOM element it references. 
+    // inputRef.current gives you the DOM element itself
+    // inputRef.current.value gives you the value that DOM element holds
+    // Every vanilla JS property that you could use can be used to manipulated
+    // CHANGING A REFERENCE VALUE DOES NOT RERENDER THE COMPONENT
+    
+    const onSubmit = e => {
+        e.preventDefault()
+        console.log(inputRef.current.value)
+        inputRef.current.value = 'Hello'
+        inputRef.current.style.backgroundColor = 'red'
+        paraRef.current.innerText = 'Goodbye'
+    }
 
-  const onSubmit = (e) => {
-    e.preventDefault()
-    console.log(inputRef.current.value)
-    inputRef.current.value = 'Hello'
-    inputRef.current.style.backgroundColor = 'red'
-    paraRef.current.innerText = 'Goodbye'
-  }
-
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <label htmlFor='name'>Name</label>
-        <input
-          type='text'
-          ref={inputRef}
-          id='name'
-          className='form-control mb-2'
-        />
-        <button type='submit' className='btn btn-primary'>
-          Submit
-        </button>
-        <p onClick={() => inputRef.current.focus()} ref={paraRef}>
-          Hello
-        </p>
-      </form>
-    </div>
-  )
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <label htmlFor='name'>Name</label>
+                <input ref={inputRef} type='text' id='name' className='form-control mb-2' />
+                <button type="submit" className='btn btn-primary'>Submit</button>
+            </form>
+            <p onClick={() => inputRef.current.focus()} ref={paraRef}>Hello</p>
+        </div>
+    )
 }
 
 export default UseRefExample1
