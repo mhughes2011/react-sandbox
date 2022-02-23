45076f45102ece933bb2cc996d3335463e53ecf5
Max Hughes <mhughes2011@gmail.com>
2022-02-22 17:26:36 -0800

    useRef example 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import UseRefExample1 from './components/UseRefExample1'
+import UseRef2 from './components/UseRef2'
 
 function App() {
   return (
     <div className="container mt-5">
       <UseRefExample1 />
+      <UseRef2 />
     </div>
   )
 }
