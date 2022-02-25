833e813880bb2d8039ede78b890e708212596f02
Max Hughes <mhughes2011@gmail.com>
2022-02-25 08:57:08 -0800

    created the useRef3 example involving how to fix memory leak

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import UseRefExample1 from './components/UseRefExample1'
 import UseRef2 from './components/UseRef2'
+import UseRef3 from './components/UseRef3'
 
 function App() {
   return (
     <div className="container mt-5">
       <UseRefExample1 />
       <UseRef2 />
+      <UseRef3 />
     </div>
   )
 }
